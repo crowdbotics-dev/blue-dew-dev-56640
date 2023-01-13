b309505fdf7ae3e826fe72034855a147b1355a2a
Crowdbotics <engineering@crowdbotics.com>
2023-01-13 05:39:17 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/blue_dew_dev_56640/MainActivity.java
@@ -0,0 +1,15 @@
+package com.blue_dew_dev_56640;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "blue_dew_dev_56640";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blue-dew-dev-56640",
+  url: "https://blue-dew-dev-56640.botics.co",
+  api: "https://blue-dew-dev-56640.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
